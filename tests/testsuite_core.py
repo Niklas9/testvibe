a57c9fdb44f3010e66901dd09a1c11946b1959ab
Niklas Andersson <nandersson900@gmail.com>
2015-11-08 17:59:28 +0000

    splitted up in pos and neg tests

--- tests/testsuite_core.py
+++ tests/testsuite_core.py
@@ -3,11 +3,15 @@ import nose
 
 import testvibe.core.cli as cli
 
+
 class TestTVCtl(object):
 
 	def test_valid_dir_name(self):
 		m = cli.CLI._is_valid_dir_name
 		assert m('asdf')
 		assert m('as-df')
+
+	def test_neg_valid_dir_name(self):
+		m = cli.CLI._is_valid_dir_name
 		assert not m('asdf$')
 		assert not m('fdsa asdf')
