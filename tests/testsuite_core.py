ecfa469d1c63b21c77e6f3df706c2f5fec1ecfaa
Niklas Andersson <nandersson900@gmail.com>
2015-11-08 15:18:43 +0000

    added validation on project name / tets groups, refactorings

---
+++ tests/testsuite_core.py
@@ -0,0 +1,13 @@
+
+import nose
+
+import testvibe.core.cli as cli
+
+class TestTVCtl(object):
+
+	def test_valid_dir_name(self):
+		m = cli.CLI._is_valid_dir_name
+		assert m('asdf')
+		assert m('as-df')
+		assert not m('asdf$')
+		assert not m('fdsa asdf')
