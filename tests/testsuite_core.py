36e6a516b966943c6c1b16a6f582c94b233adbd8
Niklas Andersson <nandersson900@gmail.com>
2015-11-20 23:03:57 +0000

    fixed test error from refactoring of cli

--- tests/testsuite_core.py
+++ tests/testsuite_core.py
@@ -1,17 +1,17 @@
 
 import nose
 
-import testvibe.core.cli as cli
+import testvibe.core.cli_handler as cli_handler
 
 
 class TestTVCtl(object):
 
 	def test_valid_dir_name(self):
-		m = cli.CLI._is_valid_dir_name
+		m = cli_handler.CLIHandler._is_valid_dir_name
 		assert m('asdf')
 		assert m('as-df')
 
 	def test_neg_valid_dir_name(self):
-		m = cli.CLI._is_valid_dir_name
+		m = cli_handler.CLIHandler._is_valid_dir_name
 		assert not m('asdf$')
 		assert not m('fdsa asdf')
