f3aa74c605c68906228f9a7ce5aaf7544e32f6c0
Niklas Andersson <nandersson900@gmail.com>
2015-11-20 23:01:08 +0000

    refactoring of tvctl/cli

--- testvibe/core/cli.py
+++ testvibe/core/cli.py
@@ -1,85 +1,51 @@
 
-import argparse
-import os
-import sys
-import re
-
 import testvibe
+import testvibe.core.cli_handler as cli_handler
 
 
 class CLI(object):
 
-    DEFAULT_INSTALL_DIR = 'default_install/'
-    FILENAME_SETTINGS = 'settings.py'
-    FILENAME_RUNLIST = 'RUNLIST'
-    FILENAME_TESTSUITE = 'test_suite.py'
-    PLACEHOLDER_NAME = '<Example>'
-    RE_VALID_DIR_NAME = '^[A-Za-z0-9-]+$'
+    parser = None
+    subparsers = None
 
-    args = None
+    def __init__(self):
+        self.parser = cli_handler.ArgumentParserWithError(
+                                        description='Testvibe control')
+        self.subparsers = self.parser.add_subparsers()
 
-    def __init__(self, args):
-        self.args = args
+    def run(self):
+        self.add_cmd_startproject()
+        self.add_cmd_testgroup()
+        self.add_cmd_testsuite()
+        self.add_cmd_run()
+        self.parser.add_argument('-V', '--version', action='version',
+                        version=testvibe.VERSION,
+                        help='show program\'s version number and exit')
+        ch = cli_handler.CLIHandler(self.parser.parse_args())
+        ch.execute()
 
-    def execute(self):
-        if self.args.cmd == 'startproject':
-            self.cmd_startproject(self.args.name)
-        elif self.args.cmd == 'addtestgroup':
-            self.cmd_addtestgroup(self.args.name)
-        elif self.args.cmd == 'run':
-            self.cmd_run()
+    def add_cmd_startproject(self):
+        self.add_cmd('startproject', 'start a new project', args=['name'])
 
-    def cmd_run(self):
-        raise NotImplementedError()
+    def add_cmd_testgroup(self):
+        self.add_cmd('addtestgroup', 'add a new test group within a project',
+                     args=['name'])
 
-    def cmd_startproject(self, name):
-        if not self._is_valid_dir_name(name):
-            sys.stderr.write('error: unsupported characters in project name\n')
-            sys.exit(2)
-        self._exit_if_dir_exists(name)
-        os.makedirs(name)
-        self._copy_file(name, CLI.FILENAME_SETTINGS,
-                       search=CLI.PLACEHOLDER_NAME, replace=name)
+    def add_cmd_testsuite(self):
+        self.add_cmd('addtestsuite', 'add a new test suite within a group',
+                     args=['name'])
 
-    def cmd_addtestgroup(self, name):
-        if not self._is_valid_dir_name(name):
-            sys.stderr.write('error: unsupported characters in test group '
-                             'name\n')
-            sys.exit(2)
-        self._exit_if_dir_exists(name)
-        os.makedirs(name)
-        self._copy_file(name, self.FILENAME_RUNLIST)
-        self._copy_file(name, self.FILENAME_TESTSUITE)
+    def add_cmd_run(self):
+        cmd = self.add_cmd('run', 'initiate test run')
+        cmd.add_argument('-p', '--parallel', default=1,
+                             help='number of testsuites to run in parallel')
+        cmd.add_argument('-v', '--verbosity', action='store_true',
+                            help='verbosity level')
 
-    @staticmethod
-    def _copy_file(dir_name, filename, search=None, replace=None):
-        with open(CLI._get_src(filename), 'r') as f:
-            src_content = f.read()
-        if search is not None and replace is not None:
-            src_content = src_content.replace(search, replace)
-        with open('%s/%s' % (dir_name, filename), 'w') as f:
-            f.write(src_content)
-
-    @staticmethod
-    def _get_src(filename):
-        return ('%s/%s%s' % (os.path.dirname(testvibe.__file__),
-                             CLI.DEFAULT_INSTALL_DIR, filename))
-
-    @staticmethod
-    def _exit_if_dir_exists(dir_name):
-         if os.path.exists(dir_name):
-            sys.stderr.write('command error: \'%s\' already exists\n'
-                             % os.path.abspath(dir_name))
-            sys.exit(2)
-
-    @staticmethod
-    def _is_valid_dir_name(s):
-        return re.match(CLI.RE_VALID_DIR_NAME, s) is not None
-
-
-class ArgumentParserWithError(argparse.ArgumentParser):
-
-    def error(self, msg):
-        sys.stderr.write('error: %s\n' % msg)
-        self.print_help()
-        sys.exit(2)
+    def add_cmd(self, name, help, args=None):
+        if args is None:  args = []
+        cmd = self.subparsers.add_parser(name, help=help)
+        for arg in args:
+            cmd.add_argument(arg)
+        cmd.set_defaults(cmd=name)
+        return cmd
---
+++ testvibe/core/cli_handler.py
@@ -0,0 +1,90 @@
+
+import argparse
+import os
+import sys
+import re
+
+import testvibe
+
+
+class CLIHandler(object):
+
+    DEFAULT_INSTALL_DIR = 'default_install/'
+    FILENAME_SETTINGS = 'settings.py'
+    FILENAME_RUNLIST = 'RUNLIST'
+    FILENAME_TESTSUITE = 'test_suite.py'
+    PLACEHOLDER_NAME = '<Example>'
+    RE_VALID_DIR_NAME = '^[A-Za-z0-9-]+$'
+
+    args = None
+
+    def __init__(self, args):
+        self.args = args
+
+    def execute(self):
+        if self.args.cmd == 'startproject':
+            self.cmd_startproject(self.args.name)
+        elif self.args.cmd == 'addtestgroup':
+            self.cmd_addtestgroup(self.args.name)
+        elif self.args.cmd == 'addtestsuite':
+            self.cmd_addtestsuite(self.args.name)
+        elif self.args.cmd == 'run':
+            self.cmd_run()
+
+    def cmd_run(self):
+        raise NotImplementedError()
+
+    def cmd_startproject(self, name):
+        if not self._is_valid_dir_name(name):
+            sys.stderr.write('error: unsupported characters in project name\n')
+            sys.exit(2)
+        self._exit_if_dir_exists(name)
+        os.makedirs(name)
+        self._copy_file(name, CLIHandler.FILENAME_SETTINGS,
+                       search=CLIHandler.PLACEHOLDER_NAME, replace=name)
+
+    def cmd_addtestsuite(self, name):
+        raise NotImplementedError()
+
+    def cmd_addtestgroup(self, name):
+        if not self._is_valid_dir_name(name):
+            sys.stderr.write('error: unsupported characters in test group '
+                             'name\n')
+            sys.exit(2)
+        self._exit_if_dir_exists(name)
+        os.makedirs(name)
+        self._copy_file(name, self.FILENAME_RUNLIST)
+        self._copy_file(name, self.FILENAME_TESTSUITE)
+
+    @staticmethod
+    def _copy_file(dir_name, filename, search=None, replace=None):
+        with open(CLIHandler._get_src(filename), 'r') as f:
+            src_content = f.read()
+        if search is not None and replace is not None:
+            src_content = src_content.replace(search, replace)
+        with open('%s/%s' % (dir_name, filename), 'w') as f:
+            f.write(src_content)
+
+    @staticmethod
+    def _get_src(filename):
+        return ('%s/%s%s' % (os.path.dirname(testvibe.__file__),
+                             CLIHandler.DEFAULT_INSTALL_DIR, filename))
+
+    @staticmethod
+    def _exit_if_dir_exists(dir_name):
+         if os.path.exists(dir_name):
+            sys.stderr.write('command error: \'%s\' already exists\n'
+                             % os.path.abspath(dir_name))
+            sys.exit(2)
+
+    @staticmethod
+    def _is_valid_dir_name(s):
+        return re.match(CLIHandler.RE_VALID_DIR_NAME, s) is not None
+
+
+class ArgumentParserWithError(argparse.ArgumentParser):
+
+    def error(self, msg):
+        sys.stderr.write('error: %s\n' % msg)
+        self.print_help()
+        sys.exit(2)
