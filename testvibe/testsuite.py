30788e36d35cff5635c65f6a8d5e3573d0e529a9
Niklas Andersson <nandersson900@gmail.com>
2015-11-28 17:19:09 +0000

    fixed some typos and python2/3 issue with internal method names

--- testvibe/testsuite.py
+++ testvibe/testsuite.py
@@ -10,7 +10,6 @@ class Testsuite(asserts.Asserts):
 
     # TODO(niklas9):
     # * make 
-    # * add logging..
     # * add wrappers for performance tests, time limits how long a test is
     #   running before it fails.. add decorator options for this?
     # * add assertions
@@ -24,7 +23,7 @@ class Testsuite(asserts.Asserts):
 
     def __init__(self):
         self.results = []
-        asserts.Asserts.__init__(self) #, self.results)
+        asserts.Asserts.__init__(self)
         self.log = logger.Log()
         self.api = api_controller.APIController(self.log)
 
@@ -35,11 +34,14 @@ class Testsuite(asserts.Asserts):
         # TODO(niklas9):
         # * timers etc here, to measure, for timeout etc
         self.setup()
-        test_id_method = '%s (%s)' % (test_method.func_name, test_id)
+        test_id_method = '%s (%s)' % (test_method.__name__, test_id)
         self.log.info('executing %s...' % test_id_method)
         try:
             test_method(*args, **kwargs)
-        except asserts.AssertionException as e:
+        # TODO(niklas9):
+        # * should catch more specific exceptions here in the future
+        #except asserts.AssertionException as e:
+        except Exception as e:
             self.log.error(e)
             self.log.debug(traceback.format_exc())
         finally:
@@ -56,9 +58,7 @@ class Testsuite(asserts.Asserts):
             self.reset_assert_counter()
 
     def setup(self):
-        # NOTE(niklas9):  to be overriden by subclass
-        pass
+        pass  # to be overriden by subclass
 
     def teardown(self):
-        # NOTE(niklas9):  to be overriden by subclass
-        pass
+        pass  # to be overriden by subclass
