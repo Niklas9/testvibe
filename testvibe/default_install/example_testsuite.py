e2962e9baaad8bedf541863ff6baa45feead01e9
Niklas Andersson <nandersson900@gmail.com>
2015-12-22 16:23:23 +0000

    fixed simpler standard testsuite example
    using github API instead of Yahoo Weather API

--- testvibe/default_install/example_testsuite.py
+++ testvibe/default_install/example_testsuite.py
@@ -1,21 +1,13 @@
 
 import testvibe
 
-import settings
-
 
 # Create your test suite and test cases here
 
 class ExampleTestsuite(testvibe.Testsuite):
 
     def test_example(self):
-        self.api.set_root_domain(settings.API_ROOT_DOMAIN)
-        url = ('yql?q=select%20*%20from%20weather.forecast%20where%20w'
-               'oeid%20in%20(select%20woeid%20from%20geo.places(1)%20w'
-               'here%20text%3D%22nome%2C%20ak%22)&format=json&env=store'
-               '%3A%2F%2Fdatatables.org%2Falltableswithkeys')
-        json, r = self.api.get(url)
+        json, r = self.api.get('https://api.github.com')
         self.assert_equal(r.status_code, 200)
-        country = json['query']['results']['channel']['location']['country']
-        self.assert_equal(country, 'United States')
-        self.assert_not_equal(country, 'Sweden')
+        self.assert_equal(json['hub_url'], 'https://api.github.com/hub')
+        self.assert_in('X-RateLimit-Limit', r.headers)
