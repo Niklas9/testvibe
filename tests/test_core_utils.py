b98d2f49342bdce00694c683e28fe5b6e3069ae2
Niklas Andersson <nandersson900@gmail.com>
2016-01-10 15:29:09 +0000

    added tests for file handler utils

--- tests/test_core_utils.py
+++ tests/test_core_utils.py
@@ -4,14 +4,75 @@ import testvibe.core.utils as utils
 
 class TestUtils(object):
 
+    def test_get_all_dirs(self):
+        m = utils.get_all_dirs
+        def listdir(path):
+            return ['..', '.', 'fdsa', 'asdf', '.fdsa']
+        def isdir(path):
+            if path.startswith('.'):  return False
+            return True
+        assert (m('/tmp', listdirs_m=listdir, isdir_m=isdir)
+                == set(('fdsa', 'asdf')))
+
     def test_get_path(self):
         m = utils.get_path
+        assert m(['a/']) == 'a/'
         assert m(['1', '2', '3']) == '1/2/3'
         assert m(('tmp', 'test/vibe', 'logs')) == 'tmp/test/vibe/logs'
         assert m(['/tmp', 'testsuite.py']) == '/tmp/testsuite.py'
+
+    def test_get_file_content(self):
+        m = utils.get_file_content
+        assert m('/tmp/asdf.txt', fh=FakeFileHandler()) == 'asdf'
+
+    def test_copy_file(self):
+        m = utils.copy_file
+        fh = FakeFileHandler()
+        m('/tmp/asdf.txt', '/tmp/fdsa.txt', search='as', replace='df', fh=fh)
+        assert fh.f_obj.enter_called and fh.f_obj.exit_called
+
+    def test_create_empty_file(self):
+        m = utils.create_empty_file
+        fh = FakeFileHandler()
+        m('/tmp/asdf.txt', fh=fh)
+        assert fh.f_obj.enter_called and fh.f_obj.exit_called
 
     def test_trim_cls_name(self):
         m = utils.trim_cls_name
         assert m('<class \'class\'>') == 'class'
         assert (m('<class \'project_template.UsersBasic\'>')
                 == 'project_template.UsersBasic')
+
+
+class FakeFileHandler(object):
+
+    f_obj = None
+
+    def __init__(self, *args, **kwargs):
+        self.f_obj = self.FakeFileObject()
+
+    def open(self, path, mode=None, encoding=None):
+        return self.f_obj
+
+    class FakeFileObject(object):
+
+        content = None
+        enter_called = None
+        exit_called = None
+
+        def __init__(self):
+            self.enter_called = False
+            self.exit_called = False
+
+        def __enter__(self):
+            self.enter_called = True
+            return self
+
+        def __exit__(self, exc_type, exc_val, exc_tb):
+            self.exit_called = True
+
+        def read(self):
+            return 'asdf'
+
+        def write(self, content):
+            self.content = content
--- testvibe/core/utils.py
+++ testvibe/core/utils.py
@@ -2,7 +2,8 @@
 import codecs
 import os
 
-
+CLS_NAME_PREFIX = '<class \''
+CLS_NAME_SUFFIX_LEN = 2
 FILEMODE_READ = 'r'
 FILEMODE_WRITE = 'w'
 FILE_ENCODING_UTF8 = 'utf-8'
@@ -10,37 +11,38 @@ STRING_BASE_DIR = '%s/'
 STRING_EMPTY = ''
 STRING_SLASH = '/'
 STRING_UNDERSCORE = '_'
-CLS_NAME_PREFIX = '<class \''
-CLS_NAME_SUFFIX_LEN = 2
 
-def get_all_dirs(path):
+def get_all_dirs(path, listdirs_m=None, isdir_m=None):
+    if listdirs_m is None:  listdirs_m = os.listdir
+    if isdir_m is None:  isdir_m = os.path.isdir
     dirs = set()
-    for f in os.listdir(path):
-        if os.path.isdir(f):
+    for f in listdirs_m(path):
+        if isdir_m(f):
             dirs.add(f)
     return dirs
 
 def get_path(fps):
     path = STRING_EMPTY.join(STRING_BASE_DIR % fp for fp in fps)
-    if path.endswith(STRING_SLASH):
-        return path[:-1]
-    return path
+    return path[:-len(STRING_SLASH)]
 
-def get_file_content(path):
-    with codecs.open(path, FILEMODE_READ, encoding=FILE_ENCODING_UTF8) as f:
+def get_file_content(path, fh=None):
+    if fh is None:  fh = codecs
+    with fh.open(path, mode=FILEMODE_READ, encoding=FILE_ENCODING_UTF8) as f:
         content = f.read()
     return content
 
-def copy_file(src, dest, search=None, replace=None):
-    with codecs.open(src, FILEMODE_READ, encoding=FILE_ENCODING_UTF8) as f:
+def copy_file(src, dest, search=None, replace=None, fh=None):
+    if fh is None:  fh = codecs
+    with fh.open(src, mode=FILEMODE_READ, encoding=FILE_ENCODING_UTF8) as f:
         src_content = f.read()
     if search is not None and replace is not None:
         src_content = src_content.replace(search, replace)
-    with codecs.open(dest, FILEMODE_WRITE, encoding=FILE_ENCODING_UTF8) as f:
+    with fh.open(dest, mode=FILEMODE_WRITE, encoding=FILE_ENCODING_UTF8) as f:
         f.write(src_content)
 
-def create_empty_file(path):
-    with codecs.open(path, FILEMODE_WRITE, encoding=FILE_ENCODING_UTF8) as f:
+def create_empty_file(path, fh=None):
+    if fh is None:  fh = codecs
+    with fh.open(path, mode=FILEMODE_WRITE, encoding=FILE_ENCODING_UTF8) as f:
         pass  # just an empty file
 
 def trim_cls_name(s):
