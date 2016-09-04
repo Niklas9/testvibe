3d11860f64744c255cc0e7c8934553ef83b9260c
Niklas Andersson <nandersson900@gmail.com>
2016-09-04 13:00:57 +0000

    removed magic value for http res 200 OK

--- testvibe/project_template/example_testsuite.py
+++ testvibe/project_template/example_testsuite.py
@@ -1,4 +1,6 @@
 
+import httplib
+
 import testvibe
 
 
@@ -8,6 +10,6 @@ class ExampleTestsuite(testvibe.Testsuite):
 
     def test_example(self):
         json, r = self.api.get('https://api.github.com')
-        self.assert_equal(r.status_code, 200)
+        self.assert_equal(r.status_code, httplib.OK)
         self.assert_equal(json['hub_url'], 'https://api.github.com/hub')
         self.assert_in('X-RateLimit-Limit', r.headers)
